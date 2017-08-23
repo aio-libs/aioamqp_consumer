edc8d8528b9b85274d37394f9a42c25d74f58d55
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-08-23 17:45:20 +0300

    Add __all__ to __init__.py (#7)

--- aioamqp_consumer/__init__.py
+++ aioamqp_consumer/__init__.py
@@ -3,3 +3,6 @@ from .exceptions import Ack, DeadLetter, Reject  # noqa isort:skip
 from .producer import Producer  # noqa isort:skip
 
 __version__ = '0.0.1'
+
+
+__all__ = ('Ack', 'Consumer', 'DeadLetter', 'Producer', 'Reject')
