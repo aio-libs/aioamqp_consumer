fa45795c12804504d30864f9f887bdd520d663b2
Dima Kruk <krkdima@gmail.com>
2018-09-20 13:48:40 +0300

    Bumb version to 0.1.3

--- aioamqp_consumer/__init__.py
+++ aioamqp_consumer/__init__.py
@@ -2,7 +2,7 @@ from .consumer import Consumer  # noqa isort:skip
 from .exceptions import Ack, DeadLetter, Reject  # noqa isort:skip
 from .producer import Producer  # noqa isort:skip
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 
 __all__ = ('Ack', 'Consumer', 'DeadLetter', 'Producer', 'Reject')
