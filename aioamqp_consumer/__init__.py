00dcd281281fe5ef65ec107a6814ce4bcb330825
hellysmile <hellysmile@gmail.com>
2020-08-09 23:01:20 +0300

    Introduce hotreload

--- aioamqp_consumer/__init__.py
+++ aioamqp_consumer/__init__.py
@@ -5,7 +5,7 @@ from .producer import Producer
 from .rpc import RpcClient, RpcMethod, RpcServer
 from .run import run
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 
 class JsonConsumer(Consumer):
--- aioamqp_consumer/run.py
+++ aioamqp_consumer/run.py
@@ -1,13 +1,26 @@
 import asyncio
+import logging
 
 from aiorun import run as _run
 
+logger = logging.getLogger(__name__)
+
 
 def run(*consumers, **kwargs):
+    debug = kwargs.pop('debug')
     loop = kwargs.pop('loop', None)
 
     if loop is None:
         loop = asyncio.get_event_loop()
+
+    kwargs['loop'] = loop
+
+    reloader = None
+    if debug:
+        import aioreloader
+        reloader = aioreloader.start()
+
+        logger.warning('Running in debug mode!!!')
 
     def shutdown_callback(loop):
         gather = asyncio.gather(*(
@@ -16,7 +29,16 @@ def run(*consumers, **kwargs):
         ), return_exceptions=True)
         loop.run_until_complete(gather)
 
+        if reloader is not None:
+            reloader.cancel()
+
+            try:
+                loop.run_until_complete(reloader)
+            except asyncio.CancelledError:
+                pass
+            except BaseException as exc:
+                logger.exception(exc)
+
     kwargs['shutdown_callback'] = shutdown_callback
-    kwargs['loop'] = loop
 
     _run(**kwargs)
